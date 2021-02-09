47cb84da06f5c01ff43acfce5eb0487151133c4f
BABABUSOLA <queenbusola1@gmail.com>
2021-02-09 07:39:40 -0800

    modification of the view contact

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def ussd_callback():
     if text == '' :
         response  = "CON What would you want to check \n"
         response += "1. My Phone Book \n"
-        response += "END 2. Add Contact" 
+        response += "2. Add Contact \n" 
     
     elif text == '1':
         for x in result.values():
@@ -37,14 +37,16 @@ def ussd_callback():
             email = x["email"]
             phoneNumber = x["phoneNumber"]
             print(name,email,phoneNumber)
-            response += f'END Name: {name} No: {phoneNumber} Email: {email}\n'
+            
+            response += f"Name:{name} No:{phoneNumber} Email:{email}\n"
+            response = "END"
 
     elif text == '2':
         response  = "CON Kindly type your number\n"
 
     elif text == f'2*{text[2:]}':
         response  = "CON Do you want to continue? \n"
-        response += "1. Yes"
+        response += "1. Yes \n"
         response += "2. No"
 
     elif text == f'2*{text[2:]}*1':
